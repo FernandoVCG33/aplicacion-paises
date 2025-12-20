25d4ad4999e5c15c5db40736925dce7e116510e7
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-19 20:48:02 -0400

    cambios 3

---
+++ country-app/src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [
+    RouterLinkActive
+  ],
+  templateUrl: './top-menu.html',
+
+})
+export class TopMenu {
+
+}
